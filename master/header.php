b4aa6b1304344ec317bf07f59e847b7a2874c03e
massif abdessamed <massif.abdessamed.11@gmail.com>
2020-10-27 19:14:50 +0100

    third c

--- master/header.php
+++ master/header.php
@@ -2,6 +2,8 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>hello</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
